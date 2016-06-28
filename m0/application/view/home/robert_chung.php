abcaacd83eae156aa5f11840295cc4228dd615ba
Robert Chung <rchung2@mail.sfsu.edu>
2016-06-28 07:55:41 +0000

    Added image to individual webpage

--- m0/application/view/home/robert_chung.php
+++ m0/application/view/home/robert_chung.php
@@ -1,4 +1,5 @@
 <div class="container">
 <h2>This is milestone 0 for team 01</h2>
 <p>This webpage was created by Robert Chung</p>
+<img src="http://cdn.wall-pix.net/albums/miscellaneous-brands/00030315.Windows.apple.jpg" alt="pic" width="50%" height="50%">
 </div>
