6e7cab9bf2d916fc8c861d17750eb2becbb32d3a
cheneric <cheneric@sfsuswe.com>
2016-07-11 02:04:23 +0000

    Created a skeleton UI for vertical prototype

--- m0/application/view/_templates/header.php
+++ m0/application/view/_templates/header.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
+<!-- Bootstrap -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Gator's List</title>
+    <!--Nav Bar-->
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Gatorslist</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li><a href="home">Home</a></li>
+          <li><a href="#">Sell Your Book</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <title>Gatorslist</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- JS -->
     <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
     <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
-
     <!-- CSS -->
     <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
 </head>
 <body>
     <!-- logo -->
     <div class="logo">
-        SU16G01
-    </div>
-
-    <!-- navigation -->
-    <div class="navigation">
-        <a href="<?php echo URL; ?>">home</a>
-        <a href="<?php echo URL; ?>home/ericchen">Eric Chen</a>
-        <a href="<?php echo URL; ?>home/robertchung">Robert Chung</a>
-        <a href="<?php echo URL; ?>home/guoyiruan">Guoyi Ruan</a>
-        <a href="<?php echo URL; ?>home/khalidalrashed">Khalid Alrashed</a>
-	<a href="<?php echo URL; ?>home/tainguyen">Tai Nguyen</a>
-	<!-- Add names to nav here! -->
+        Gatorslist
     </div>
