ec8a68f8c9dec97d1dbea67da9984b748591007e
Alrashed <Khalid@Khalids-MBP.T-mobile.com>
2016-07-10 23:56:46 -0700

    Remove unnecessary text from home page

--- m0/application/view/home/index.php
+++ m0/application/view/home/index.php
@@ -1,7 +1,6 @@
 <div class="container">
 
   <h2>You are in the View: application/view/home/index.php (everything in the box comes from this file)</h2>
-  <p>Please remove this when done with vertical prototyping for presentation.</p>
   <div class="row">
     <div class="col-lg-6">
       <div class="input-group">
