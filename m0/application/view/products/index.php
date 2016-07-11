5e88fb3272a1b9ed9cc61489a894dc3a8b6f2584
Alrashed <Khalid@Khalids-MBP.T-mobile.com>
2016-07-10 23:49:22 -0700

    Change search description text

--- m0/application/view/products/index.php
+++ m0/application/view/products/index.php
@@ -2,7 +2,7 @@
     <h2>You are in the View: application/view/products/index.php (everything in this box comes from that file)</h2>
     <!-- add song form -->
     <div class="box">
-        <h3>Search an item</h3>
+        <h3>Search an item (book,novel,dictionary,encyclopedia,shorts)<h3>
         <form action="<?php echo URL; ?>products/searchproducts" method="POST">
             <label>Item Name: </label>
             <input type="text" name="searchinput" value="" required />
