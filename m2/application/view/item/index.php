3e7de2a7bd8ae32f07b0f901b642ad7479821f89
cheneric <cheneric@sfsuswe.com>
2016-07-27 09:05:15 +0000

    Added contact seller page

--- m2/application/view/item/index.php
+++ m2/application/view/item/index.php
@@ -10,7 +10,7 @@
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nisi sit amet pulvinar finibus. Ut lacinia magna ac urna venenatis placerat. Donec elementum bibendum lectus et ultricies. Praesent eleifend lorem sed placerat fermentum. Quisque ac sem congue, sodales nisi feugiat, euismod tellus. Integer et laoreet sapien, ut egestas velit. Nam suscipit dui ut quam mattis laoreet. Donec nulla justo, hendrerit vitae lobortis nec, elementum feugiat turpis. Fusce non tellus porttitor, vestibulum justo sit amet, vulputate eros. Cras id metus vitae risus ultricies interdum. Cras porttitor purus ac magna molestie fringilla. Suspendisse semper faucibus justo in vehicula. </p>
 </div>
   <h2>Seller: "Username" Rating: "5/5"</h2>
-  <p><a class="btn btn-primary btn-lg" href="#" role="button">Contact Seller</a></p>
+  <p><a class="btn btn-primary btn-lg" href="<?php echo URL; ?>contact/index" role="button">Contact Seller</a></p>
   <p>"Contact the seller to arrange a time to meet up and purchase the item."</p>
   <p><a class="btn btn-primary btn-lg" href="#" role="button">Pay in Advance</a></p>
   <p>"Pay in advance and get in touch with the seller to arrange a time and pick up the item."</p>
