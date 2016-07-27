e4dbdfa930c641379ced2185da496726643f5830
cheneric <cheneric@sfsuswe.com>
2016-07-27 08:53:48 +0000

    added some ui to results

--- m2/application/view/products/product.php
+++ m2/application/view/products/product.php
@@ -1,41 +1,33 @@
 <div class="container">
-    <h2>You are in the View: application/view/products/product.php (everything in this box comes from that file)</h2>
     <div class="box">
         <h3>Search Results:</h3>
 <!--        <form action="--><?php //echo URL; ?><!--products/searchProducts" method="POST">-->
         <table>
             <thead style="background-color: #ddd; font-weight: bold;">
             <tr>
-                <td>Image</td>
-<!--                <td>ID</td>-->
-                <td>Title</td>
-                <td>Description</td>
-                <td>Price</td>
-                <td>Condition</td>
-                <td>Date</td>
-<!--                <td>Detail</td>-->
 
             </tr>
             </thead>
             <tbody>
             <?php foreach ($products as $product) { ?>
                 <tr>
-                    <td><?php if (isset($product->Image_blob1) && $product->Image_blob1 != "") echo '<img src="data:image/jpeg;base64,'.base64_encode($product->Image_blob1).'"  height="100" width="100" />'; ?>
-
-<!--                    <td>--><?php //if (isset($product->Product_id)) echo htmlspecialchars($product->Product_id, ENT_QUOTES, 'UTF-8'); ?><!--</td>-->
-
-                    <td><?php if (isset($product->Title)) echo htmlspecialchars($product->Title, ENT_QUOTES, 'UTF-8'); ?></td>
-                    
-                    <td><?php if (isset($product->Description)) echo htmlspecialchars($product->Description, ENT_QUOTES, 'UTF-8'); ?></td>
-                    
-                    <td><?php if (isset($product->Price)) echo "$" . htmlspecialchars($product->Price, ENT_QUOTES, 'UTF-8'); ?></td>
-                    
-                    <td><?php if (isset($product->ItemCondition)) echo htmlspecialchars($product->ItemCondition, ENT_QUOTES, 'UTF-8'); ?></td>
-
-                    <td><?php if (isset($product->Postdate)) echo htmlspecialchars($product->Postdate, ENT_QUOTES, 'UTF-8'); ?></td>
-                    
-<!--                    <td><a href="--><?php //echo URL . 'sell/getItem/' . htmlspecialchars($product->Product_id, ENT_QUOTES, 'UTF-8'); ?><!--">View Detail</a></td>-->
                 </tr>
+
+                    <div class="col-sm-4">
+                <div class="panel-body"><img style="margin-left: 10%;"<?php if (isset($product->Image_blob1) && $product->Image_blob1 != "") echo 'src="data:image/jpeg;base64,'.base64_encode($product->Image_blob1).'"  height="100" width="100" />'; else echo 'src="http://placehold.it/150x80?text=IMAGE"' ?></div>
+                <div class="panel-body meta-style" style="float:left; margin-top: -5%; width:100%">
+                     <div class=" " style="float:left">Item Name</div>
+                     <div class=" " style="float:left; margin-left: 5%"> : <?php if (isset($product->Title)) echo htmlspecialchars($product->Title, ENT_QUOTES, 'UTF-8'); ?></div></br>
+                     <div class=" " style="float:left">Price </div>
+                     <div class=" " style="float:left; margin-left: 16%"> : <?php if (isset($product->Price)) echo "$" . htmlspecialchars($product->Price, ENT_QUOTES, 'UTF-8'); ?></div></br>
+                     <div class=" " style="float:left">Item Condition </div>
+                     <div class=" " style="float:left; margin-left: 16%"> : <?php if (isset($product->ItemCondition)) echo htmlspecialchars($product->ItemCondition, ENT_QUOTES, 'UTF-8'); ?></div></br>
+                     <div class=" " style="float:left">Date Upload : </div>
+                     <div class=" " style="float:left; margin-left: 1%"> <?php if (isset($product->Postdate)) echo htmlspecialchars($product->Postdate, ENT_QUOTES, 'UTF-8'); ?></div></br></br>
+                     <div style="text-align:center; width:100%; margin-left: -15%;"><a type="button" class="btn btn-primary btn-md " href="<?php echo URL; ?>item/index" >View item detail</a></div>
+                </div>
+        
+            </div>
             <?php } ?>
             </tbody>
         </table>
