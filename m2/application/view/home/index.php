86651ffd568839c34a9df56f547647388fbadaa5
cheneric <cheneric@sfsuswe.com>
2016-08-01 01:56:10 +0000

    Added quickbuy modal

--- m2/application/view/home/index.php
+++ m2/application/view/home/index.php
@@ -40,10 +40,31 @@
                          <div class=" " style="float:left; margin-left: 1%"> {Retrieve date uploaded}</div></br></br>
                          <div class="row col-lg-offset-2">		                         
                          <div class="col-lg-offset-0" style="float:left;"><form action="<?php echo URL; ?>item/index"><button class="view-item-button" >View Item</button></div></form>		
-                         <div class="col-lg-offset-0" style="float:left" id="contact"><form action="<?php echo URL; ?>item/index"><button class="contact-seller-button"  >Contact Seller</button></div></form>		
-                         </div>
+                         <div class="col-lg-offset-0" style="float:left" id="contact"><button class="contact-seller-button" data-toggle="modal" data-target="#quickBuy" >Quick Buy</button></div></form>
+                            <!-- Modal -->                      
+                                <div class="modal fade" id="quickBuy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden=   "true">
+                                    <div class="vertical-alignment-helper">
+                                        <div class="modal-dialog vertical-align-center">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;   </span><span class="sr-only"                   >Close</span>
+
+                                                    </button>
+                                                    <h4 class="modal-title text-center" id="myModalLabel">Quick Buy</h4>
+
+                                                </div>
+                                                <div class="modal-body text-center">
+                                                    <a href="<?php echo URL; ?>contact/seller"><button type="button" class="btn btn-primary">Contact Seller</button></a>
+                                                    <p>"Contact the seller to arrange a time to meet up and purchase the item."</p>
+                                                    <a href="<?php echo URL; ?>item/index"><button type="button" class="btn btn-primary">Pay in Advance</button></a>
+                                                    <p>"Pay in advance and get in touch with the seller to arrange a time and pick up the item."</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>	<!-- Modal -->
+                            </div>
                     </div>
-                    
                 </div>
                 <div class="col-sm-4">
                     <div class="panel-body"><a href="<?php echo URL; ?>item/index"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></a>
@@ -57,10 +78,31 @@
                          <div class=" " style="float:left; margin-left: 1%"> {Retrieve date uploaded}</div></br></br>
                          <div class="row col-lg-offset-2">		                         
                          <div class="col-lg-offset-0" style="float:left;"><form action="<?php echo URL; ?>item/index"><button class="view-item-button" >View Item</button></div></form>		
-                         <div class="col-lg-offset-0" style="float:left" id="contact"><form action="<?php echo URL; ?>item/index"><button class="contact-seller-button"  >Contact Seller</button></div></form>		
-                         </div>
+                         <div class="col-lg-offset-0" style="float:left" id="contact"><button class="contact-seller-button" data-toggle="modal" data-target="#quickBuy" >Quick Buy</button></div></form>
+                            <!-- Modal -->                      
+                                <div class="modal fade" id="quickBuy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden=   "true">
+                                    <div class="vertical-alignment-helper">
+                                        <div class="modal-dialog vertical-align-center">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;   </span><span class="sr-only"                   >Close</span>
+
+                                                    </button>
+                                                    <h4 class="modal-title text-center" id="myModalLabel">Quick Buy</h4>
+
+                                                </div>
+                                                <div class="modal-body text-center">
+                                                    <a href="<?php echo URL; ?>contact/seller"><button type="button" class="btn btn-primary">Contact Seller</button></a>
+                                                    <p>"Contact the seller to arrange a time to meet up and purchase the item."</p>
+                                                    <a href="<?php echo URL; ?>item/index"><button type="button" class="btn btn-primary">Pay in Advance</button></a>
+                                                    <p>"Pay in advance and get in touch with the seller to arrange a time and pick up the item."</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>  <!-- Modal -->
+                            </div>
                     </div>
-            
                 </div>
                 <div class="col-sm-4">
                     <div class="panel-body"><a href="<?php echo URL; ?>item/index"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></a>
@@ -74,11 +116,32 @@
                          <div class=" " style="float:left; margin-left: 1%"> {Retrieve date uploaded}</div></br></br>
                          <div class="row col-lg-offset-2">		                         
                          <div class="col-lg-offset-0" style="float:left;"><form action="<?php echo URL; ?>item/index"><button class="view-item-button" >View Item</button></div></form>		
-                         <div class="col-lg-offset-0" style="float:left" id="contact"><form action="<?php echo URL; ?>item/index"><button class="contact-seller-button"  >Contact Seller</button></div></form>		
-                         </div>
+                         <div class="col-lg-offset-0" style="float:left" id="contact"><button class="contact-seller-button" data-toggle="modal" data-target="#quickBuy" >Quick Buy</button></div></form>
+                            <!-- Modal -->                      
+                                <div class="modal fade" id="quickBuy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden=   "true">
+                                    <div class="vertical-alignment-helper">
+                                        <div class="modal-dialog vertical-align-center">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;   </span><span class="sr-only"                   >Close</span>
+
+                                                    </button>
+                                                    <h4 class="modal-title text-center" id="myModalLabel">Quick Buy</h4>
+
+                                                </div>
+                                                <div class="modal-body text-center">
+                                                    <a href="<?php echo URL; ?>contact/seller"><button type="button" class="btn btn-primary">Contact Seller</button></a>
+                                                    <p>"Contact the seller to arrange a time to meet up and purchase the item."</p>
+                                                    <a href="<?php echo URL; ?>item/index"><button type="button" class="btn btn-primary">Pay in Advance</button></a>
+                                                    <p>"Pay in advance and get in touch with the seller to arrange a time and pick up the item."</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>  <!-- Modal -->
+                            </div>
                     </div>
                 </div>
-           </div>
            <div class="row">
             <div class="col-sm-4">
                 <div class="panel-body"><a href="<?php echo URL; ?>item/index"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></a>
@@ -92,11 +155,32 @@
                      <div class=" " style="float:left; margin-left: 1%"> {Retrieve date uploaded}</div></br></br>
                      <div class="row col-lg-offset-2">		                         
                      <div class="col-lg-offset-0" style="float:left;"><form action="<?php echo URL; ?>item/index"><button class="view-item-button" >View Item</button></div></form>		
-                     <div class="col-lg-offset-0" style="float:left" id="contact"><form action="<?php echo URL; ?>item/index"><button class="contact-seller-button"  >Contact Seller</button></div></form>		
-                     </div>
+                     <div class="col-lg-offset-0" style="float:left" id="contact"><button class="contact-seller-button" data-toggle="modal" data-target="#quickBuy" >Quick Buy</button></div></form>
+                            <!-- Modal -->                      
+                                <div class="modal fade" id="quickBuy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden=   "true">
+                                    <div class="vertical-alignment-helper">
+                                        <div class="modal-dialog vertical-align-center">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;   </span><span class="sr-only"                   >Close</span>
+
+                                                    </button>
+                                                    <h4 class="modal-title text-center" id="myModalLabel">Quick Buy</h4>
+
+                                                </div>
+                                                <div class="modal-body text-center">
+                                                    <a href="<?php echo URL; ?>contact/seller"><button type="button" class="btn btn-primary">Contact Seller</button></a>
+                                                    <p>"Contact the seller to arrange a time to meet up and purchase the item."</p>
+                                                    <a href="<?php echo URL; ?>item/index"><button type="button" class="btn btn-primary">Pay in Advance</button></a>
+                                                    <p>"Pay in advance and get in touch with the seller to arrange a time and pick up the item."</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>  <!-- Modal -->
+                            </div>
+                    </div>
                 </div>
-                
-            </div>
             <div class="col-sm-4">
                 <div class="panel-body"><a href="<?php echo URL; ?>item/index"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></a>
                     </div>
@@ -109,11 +193,32 @@
                      <div class=" " style="float:left; margin-left: 1%"> {Retrieve date uploaded}</div></br></br>
                      <div class="row col-lg-offset-2">		                         
                          <div class="col-lg-offset-0" style="float:left;"><form action="<?php echo URL; ?>item/index"><button class="view-item-button" >View Item</button></div></form>		
-                         <div class="col-lg-offset-0" style="float:left" id="contact"><form action="<?php echo URL; ?>item/index"><button class="contact-seller-button"  >Contact Seller</button></div></form>		
-                     </div>
+                         <div class="col-lg-offset-0" style="float:left" id="contact"><button class="contact-seller-button" data-toggle="modal" data-target="#quickBuy" >Quick Buy</button></div></form>
+                            <!-- Modal -->                      
+                                <div class="modal fade" id="quickBuy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden=   "true">
+                                    <div class="vertical-alignment-helper">
+                                        <div class="modal-dialog vertical-align-center">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;   </span><span class="sr-only"                   >Close</span>
+
+                                                    </button>
+                                                    <h4 class="modal-title text-center" id="myModalLabel">Quick Buy</h4>
+
+                                                </div>
+                                                <div class="modal-body text-center">
+                                                    <a href="<?php echo URL; ?>contact/seller"><button type="button" class="btn btn-primary">Contact Seller</button></a>
+                                                    <p>"Contact the seller to arrange a time to meet up and purchase the item."</p>
+                                                    <a href="<?php echo URL; ?>item/index"><button type="button" class="btn btn-primary">Pay in Advance</button></a>
+                                                    <p>"Pay in advance and get in touch with the seller to arrange a time and pick up the item."</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>  <!-- Modal -->
+                            </div>
+                    </div>
                 </div>
-        
-            </div>
             <div class="col-sm-4">
                 <div class="panel-body"><a href="<?php echo URL; ?>item/index"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></a>
                     </div>
@@ -126,8 +231,32 @@
                      <div class=" " style="float:left; margin-left: 1%"> {Retrieve date uploaded}</div></br></br>
                      <div class="row col-lg-offset-2">		                         
                          <div class="col-lg-offset-0" style="float:left;"><form action="<?php echo URL; ?>item/index"><button class="view-item-button" >View Item</button></div></form>		
-                         <div class="col-lg-offset-0" style="float:left" id="contact"><form action="<?php echo URL; ?>item/index"><button class="contact-seller-button"  >Contact Seller</button></div></form>		
-                     </div>
+                         <div class="col-lg-offset-0" style="float:left" id="contact"><button class="contact-seller-button" data-toggle="modal" data-target="#quickBuy" >Quick Buy</button></div></form>
+                            <!-- Modal -->                      
+                                <div class="modal fade" id="quickBuy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden=   "true">
+                                    <div class="vertical-alignment-helper">
+                                        <div class="modal-dialog vertical-align-center">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;   </span><span class="sr-only"                   >Close</span>
+
+                                                    </button>
+                                                    <h4 class="modal-title text-center" id="myModalLabel">Quick Buy</h4>
+
+                                                </div>
+                                                <div class="modal-body text-center">
+                                                    <a href="<?php echo URL; ?>contact/seller"><button type="button" class="btn btn-primary">Contact Seller</button></a>
+                                                    <p>"Contact the seller to arrange a time to meet up and purchase the item."</p>
+                                                    <a href="<?php echo URL; ?>item/index"><button type="button" class="btn btn-primary">Pay in Advance</button></a>
+                                                    <p>"Pay in advance and get in touch with the seller to arrange a time and pick up the item."</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>  <!-- Modal -->
+                            </div>
+                    </div>
+                </div>
                 </div>
             </div>
           </div>
