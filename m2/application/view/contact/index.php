86dd568dca4e3decba90e0d5cfd99dc51b1db197
Alrashed <Khalid@Khalids-MBP.T-mobile.com>
2016-08-05 21:10:24 -0700

    rename m3 directory to gatorslist and add M3 and M4 docs

--- m2/application/view/contact/index.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-<form>
-    <label for="exampleTextarea">Message to "Seller"</label>
-    <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
-  </fieldset>
-  <a type="submit" class="btn btn-warning btn-lg" href="<?php echo URL; ?>contact/sent">Submit</a>
-</form>
-</div>
