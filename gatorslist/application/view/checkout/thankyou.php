358f86a9663dbb1e0688f6063cf5573504a07cb8
Tai Nguyen <tnguye21@mail.sfsu.edu>
2016-08-08 22:26:31 +0000

    Style up logout page and thankyou page

--- gatorslist/application/view/checkout/thankyou.php
+++ gatorslist/application/view/checkout/thankyou.php
@@ -1,13 +1,11 @@
 <META http-equiv="refresh" content="5;URL=<?php echo URL; ?>">
 
-<div class="jumbotron">
-  <h1 class="text-center">Order has been placed!</h1>
-  <h1 class="text-center">Your message has been sent to the seller.</h1>
+<div class="container col-lg-8 col-lg-offset-2 text-center logout" >
+  <h3>Order has been placed!</h4>
+  <h4>Your message has been sent to the seller.</h4>
 
     <div class="box text-center"> 
         <p><strong>Feel free to shop around more!</strong></p>
         <p><strong>You will be redirected to the home page in 5 seconds. Or click <a href="<?php echo URL; ?>">here</a> to go there now.</strong></p>
     </div>
-</div>
-
 </div>
--- gatorslist/application/view/logout/index.php
+++ gatorslist/application/view/logout/index.php
@@ -1,10 +1,9 @@
 <META http-equiv="refresh" content="5;URL=<?php echo URL; ?>">
 
-<div class="container">
-    <h2>You have been logged out</h2>
-    
-    <div class="box"> 
+<div class="container col-lg-8 col-lg-offset-2 text-center logout" style="margin-top:5vh;margin-bottom:10vh;background-color:#cfebfd" >
+    <h4>You have been logged out!</h4>
+    <div class="box"><i>
         <p><strong>You've logged out. See you again soon!</strong></p>
-        <p><strong>You will be redirected to the home page in 5 seconds. Or click <a href="<?php echo URL; ?>">here</a> to go there now.</strong></p>
+        <p><strong>You will be redirected to the home page in 5 seconds. Or click <a href="<?php echo URL; ?>">here</a> to go there now.</strong></p></i>
     </div>
 </div>
