bc5a078c31c76c793bb96e0a116a6da0b3b6fad9
cheneric <cheneric@sfsuswe.com>
2016-08-09 18:56:30 +0000

    Moved item title for increased readability

--- gatorslist/application/view/item/index.php
+++ gatorslist/application/view/item/index.php
@@ -1,6 +1,5 @@
 
 <div class="row" style="width:80%; margin-left:10%;opacity: 1;border-radius: 5px;padding-bottom: 5vh;">
-  	  <div class="panel-heading  col-xs-12"  style="background: #f2f2f2" ><h1 class=" col-xs-offset-1"  style="border-radius:10px"><?php if (isset($productDetail->Title)) echo htmlspecialchars($productDetail->Title, ENT_QUOTES, 'UTF-8'); ?></h1></div>
       <div class="row panel-body" style="background: #f2f2f2" >
       <div class="col-xs-6 col-xs-offset-1 float-left panel-body"  style="height: 60vh; width: 30vw;">
         <a href="#" class="thumbnail" >
@@ -15,7 +14,7 @@
         </a>
       </div>
       		<div class="float-left" style="background: #f2f2f2;height: 60vh; width: 30vw;">
-              
+              <h1><?php if (isset($productDetail->Title)) echo htmlspecialchars($productDetail->Title, ENT_QUOTES, 'UTF-8'); ?></h1>
               <h5>Price &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <b>$<?php if (isset($productDetail->Price)) echo htmlspecialchars($productDetail->Price, ENT_QUOTES, 'UTF-8'); ?></b></h5>
               <h5>Condition: <i><?php if (isset($productDetail->ItemCondition)) echo htmlspecialchars($productDetail->ItemCondition, ENT_QUOTES, 'UTF-8'); ?></i></h5>
               <h5>Seller&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?php if (isset($productDetail->Email)) echo htmlspecialchars($productDetail->Email, ENT_QUOTES, 'UTF-8'); ?></h5>
