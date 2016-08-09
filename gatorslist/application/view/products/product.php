3152c6de2b31d746f4e36ad958edbbbf21a85df9
cheneric <cheneric@sfsuswe.com>
2016-08-09 06:10:03 +0000

    Removed time stamp from search result page

--- gatorslist/application/view/products/product.php
+++ gatorslist/application/view/products/product.php
@@ -112,7 +112,7 @@
                      <div class=" " style="float:left">Item Condition </div>
                      <div class=" " style="float:left;"> : <?php if (isset($product->ItemCondition)) echo htmlspecialchars($product->ItemCondition, ENT_QUOTES, 'UTF-8'); ?></div></br>
                      <div class=" " style="float:left">Date Upload : </div>
-                     <div class=" " style="float:left;"> <?php if (isset($product->Postdate)) echo htmlspecialchars($product->Postdate, ENT_QUOTES, 'UTF-8'); ?></div></br></br>
+                     <div class=" " style="float:left;"> <?php if (isset($product->Postdate)) echo htmlspecialchars(date("Y-m-d", strtotime($product->Postdate)), ENT_QUOTES, 'UTF-8'); ?></div></br></br>
                      <div class="row col-lg-offset-2">                               
                          <div class="col-lg-offset-0" style="float:left;"><form action="<?php echo URL . 'item/showitem/' . htmlspecialchars($product->Product_id, ENT_QUOTES, 'UTF-8'); ?>"><button class="view-item-button" >View Item</button></div></form>
                          <div class="col-lg-offset-0" style="float:left" id="contact"><form action="<?php echo URL. 'confirm/showitem/' . htmlspecialchars($product->Product_id, ENT_QUOTES, 'UTF-8'); ?>"><button class="contact-seller-button">Buy Now</button></div></form>
