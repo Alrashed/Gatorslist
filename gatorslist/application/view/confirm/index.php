19d0fc13752bd6351357c0b3ca33e57d360988f4
Tai Nguyen <tnguye21@mail.sfsu.edu>
2016-08-09 16:14:52 +0000

    Critical update item page

--- gatorslist/application/view/confirm/index.php
+++ gatorslist/application/view/confirm/index.php
@@ -2,12 +2,12 @@
 <div class="panel-heading shadow" style="background:#d0d0d0"><h1 class="text-center" style="color:#000000;">Order Confirmation</h1></div>
     <div class="jumbotron panel-body" >         
     	<div class="row">
-            <div class="col-lg-2 col-lg-offset-2">
+            <div class="col-lg-2 col-lg-offset-1">
                           <img  <?php if (isset($productDetail->Image_blob1) && $productDetail->Image_blob1 != "")
                           echo 'src="data:image/jpeg;base64,'.base64_encode($productDetail->Image_blob1).'" height="350" width="300"';?>>
             </div>
 
-            <div class="col-lg-7 text-center">
+            <div class="col-lg-7 col-lg-offset-1 text-center">
             <h4><b><?php if (isset($productDetail->Title)) echo htmlspecialchars($productDetail->Title, ENT_QUOTES, 'UTF-8'); ?> - $<?php if (isset($productDetail->Price)) echo htmlspecialchars($productDetail->Price, ENT_QUOTES, 'UTF-8'); ?></b></h4><hr>
             <p><a class="btn btn-warning btn-lg" href="<?php echo URL; ?>contact/index" role="button"><b>Contact Seller</b></a></p>
             <p style="font-size:14px">"Contact the seller to arrange a time to meet up and purchase the item."</p>
